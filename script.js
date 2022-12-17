fc0f434c3ea619e6f04472a85ec32d52b1850841
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2022-12-17 23:12:59 +0100

    Solución error loader teléfono

--- script.js
+++ script.js
@@ -7,19 +7,21 @@ function sleep(ms) {
 load = async function () {
     const s = Snap();
     var x = 0
-    var maskfondo = s.rect(0, 0, parseInt(window.innerWidth), parseInt(window.innerHeight))
+    var width = parseInt(window.innerWidth)
+    var height = parseInt(window.innerHeight)
+    var maskfondo = s.rect(0, 0, width, height)
     maskfondo.attr({
         fill: "white"
     });
-    var circulo = s.circle(window.innerWidth / 2, window.innerHeight / 2, 0);
+    var circulo = s.circle(window.innerWidth / 2, height / 2, 0);
     mascara = s.group(maskfondo, circulo);
-    var fondo = s.rect(0, 0, parseInt(window.innerWidth), parseInt(window.innerHeight))
+    var fondo = s.rect(0, 0, width, height)
     fondo.attr({
         mask: mascara,
         fill: "#000"
     });
     for (x = 0; x < 3; x++) {
-        var circ = s.circle(window.innerWidth / 2, window.innerHeight / 2, 0);
+        var circ = s.circle(width / 2, height / 2, 0);
         circ.animate({
             r: 0
         }, 0);
@@ -29,13 +31,13 @@ load = async function () {
             strokeWidth: 25
         });
         circ.animate({
-            r: Math.max(parseInt(window.innerHeight/1.75), parseInt(window.innerWidth/1.75))
+            r: Math.max(parseInt(height/1.75), parseInt(width/1.75))
         }, t);
         await sleep(t)
         circ.remove()
     }
     circulo.animate({
-        r: Math.max(parseInt(window.innerHeight/1.75), parseInt(window.innerWidth/1.75))
+        r: Math.max(parseInt(height/1.75), parseInt(width/1.75))
     }, t)
     return new Promise(resolve => setTimeout(resolve, t+200))
 }
